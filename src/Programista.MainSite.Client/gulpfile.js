a59b725b171366142952981b1c23ccc7253bc126
Roman <kowaliszyn@gazeta.pl>
2019-04-06 18:59:30 +0200

    Add client libraries [#2]

---
+++ src/Programista.MainSite.Client/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+    files: [
+        'node_modules/@fortawesome/**/fontawesome-free/css/*.*',
+        'node_modules/@fortawesome/**/fontawesome-free/js/*.*',
+        'node_modules/@fortawesome/**/fontawesome-free/webfonts/*.*',
+
+        'node_modules/*/dist/css/*.*',
+        'node_modules/*/dist/js/*.*'
+    ]
+};
+
+gulp.task('clean', function () {
+    return del(['wwwroot/libs/**/*']);
+});
+
+gulp.task('default', function () {
+    gulp.src(paths.files).pipe(gulp.dest('wwwroot/libs'));
+});
